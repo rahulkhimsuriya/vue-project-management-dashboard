45ab99ab780bb458ba77a0070e9594769d093688
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-08-22 17:16:10 +0530

    create card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app" class="bg-white">
-    <aside class="w-56 border-r">
+  <div id="app" class="bg-white flex">
+    <aside class="w-64 border-r">
       <header class="flex flex-col justify-between min-h-screen">
         <div class="mt-6">
           <h1 class="px-6 text-3xl font-bold">S</h1>
@@ -153,7 +153,245 @@
         </div>
       </header>
     </aside>
-    <main></main>
+    <main class="px-10 w-full">
+      <header class="py-6 flex items-center justify-between">
+        <div>
+          <div class="relative">
+            <span class="absolute inset-y-0 pl-4 py-3 text-gray-400">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </span>
+            <input
+              type="text"
+              class="w-64 border pl-10 pr-4 py-2 text-sm leading-none text-gray-800 focus:border-gray-800 focus:bg-gray-100 rounded-lg"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <div class="flex space-x-10">
+          <div class="flex space-x-6">
+            <a href="#">
+              <span>
+                <svg
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="w-6 h-6 text-gray-600"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                  />
+                </svg>
+              </span>
+            </a>
+            <a href="#">
+              <span>
+                <svg
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="w-6 h-6 text-gray-600"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  ></path>
+                </svg>
+              </span>
+            </a>
+          </div>
+          <div class="flex flex-col">
+            <time
+              datetime="17:30"
+              class="text-2xl text-gray-900 font-normal leading-none"
+              >17:30</time
+            >
+            <time
+              datetime="2020-08-18"
+              class="mt-1 text-sm text-gray-900 font-normal leading-none"
+              >18 August</time
+            >
+          </div>
+        </div>
+      </header>
+
+      <div class="flex">
+        <div class="w-3/5">
+          <div>
+            <h2 class="text-base font-semibold text-gray-900">
+              Recent Folders
+            </h2>
+
+            <div class="mt-4 flex space-x-6">
+              <div
+                class="w-32 h-32 flex flex-col items-center justify-center bg-blue-100 rounded-lg"
+              >
+                <span>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    class="w-16 h-16 text-blue-400"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                    />
+                  </svg>
+                </span>
+                <p class="text-sm font-semibold text-gray-800">Invoices</p>
+              </div>
+
+              <div
+                class="w-32 h-32 relative flex flex-col items-center justify-center bg-blue-100 rounded-lg"
+              >
+                <div
+                  class="absolute top-0 right-0 flex items-center justify-center -mt-1 -mr-1 w-4 h-4 bg-white rounded-full"
+                >
+                  <span class="block w-2 h-2 bg-pink-400 rounded-full"></span>
+                </div>
+
+                <span>
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    class="w-16 h-16 text-blue-400"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                    />
+                  </svg>
+                </span>
+                <p class="text-sm font-semibold text-gray-800">Customers</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="mt-16">
+            <h2 class="text-base font-semibold text-gray-900">
+              Projects
+            </h2>
+
+            <div class="mt-4 flex items-center justify-between gap-4">
+              <div class="w-1/2 p-6 flex bg-blue-100">
+                <div
+                  class="h-8 flex items-center bg-white text-center rounded-full"
+                >
+                  <span class="p-3 font-bold text-purple-400">S</span>
+                </div>
+                <div class="ml-2 w-full">
+                  <p class="text-base font-semibold text-gray-900">Sintext</p>
+                  <div class="mt-4">
+                    <div
+                      class="relative w-full h-2 bg-white overflow-hidden rounded-full"
+                    >
+                      <div
+                        class="absolute left-0 top-0 h-full w-9/12 bg-blue-400"
+                      ></div>
+                    </div>
+                    <span class="text-xs font-semibold text-gray-800">
+                      26/47
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="w-1/2 p-6 flex bg-blue-100">
+                <div
+                  class="h-8 flex items-center bg-white text-center rounded-full"
+                >
+                  <span class="p-3 font-bold text-red-400">T</span>
+                </div>
+                <div class="ml-2 w-full">
+                  <p class="text-base font-semibold text-gray-900">Tetrion</p>
+                  <div class="mt-4">
+                    <div
+                      class="relative w-full h-2 bg-white overflow-hidden rounded-full"
+                    >
+                      <div
+                        class="absolute left-0 top-0 h-full w-4/12 bg-blue-400"
+                      ></div>
+                    </div>
+                    <span class="text-xs font-semibold text-gray-800">
+                      7/21
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="mt-4 flex justify-between gap-4">
+              <div class="w-1/2 p-6 flex bg-blue-100">
+                <div
+                  class="h-8 flex items-center bg-white text-center rounded-full"
+                >
+                  <span class="p-3 font-bold text-green-400">P</span>
+                </div>
+                <div class="ml-2 w-full">
+                  <p class="text-base font-semibold text-gray-900">Petron</p>
+                  <div class="mt-4">
+                    <div
+                      class="relative w-full h-2 bg-white overflow-hidden rounded-full"
+                    >
+                      <div
+                        class="absolute left-0 top-0 h-full w-11/12 bg-blue-400"
+                      ></div>
+                    </div>
+                    <span class="text-xs font-semibold text-gray-800">
+                      12/16
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class="w-1/2 p-6 flex flex-col items-center justify-center bg-white border"
+              >
+                <span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    class="w-6 h-6 text-gray-600"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    />
+                  </svg>
+                </span>
+                <p class="text-gray-500">Add new</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-2/5"></div>
+      </div>
+    </main>
   </div>
 </template>
 
