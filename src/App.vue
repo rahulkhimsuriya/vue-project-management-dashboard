884f1bacdfc13b12cb4ef1ec022a375ea166cf86
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-08-22 18:47:48 +0530

    tweak

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="bg-white flex">
+  <div id="app" class="bg-white flex min-h-screen">
     <aside class="w-64 border-r">
-      <header class="flex flex-col justify-between min-h-screen">
+      <header class="flex flex-col justify-between h-full">
         <div class="mt-6">
           <h1 class="px-6 text-3xl font-bold">S</h1>
 
@@ -187,12 +187,12 @@
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  class="w-6 h-6 text-gray-600"
+                  class="w-6 h-6 text-gray-800"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
+                    stroke-width="1.5"
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
@@ -204,12 +204,12 @@
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  class="w-6 h-6 text-gray-600"
+                  class="w-6 h-6 text-gray-800"
                 >
                   <path
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    stroke-width="2"
+                    stroke-width="1.5"
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   ></path>
                 </svg>
